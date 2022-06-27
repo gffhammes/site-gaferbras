22a08c1f65e673042179b9795027a72aa12a880f
Guilherme Hammes <gffhammes@gmail.com>
2022-06-27 10:52:34 -0300

    feat:add progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { HeadPageTitle } from '../src/components/common/HeadPageTitle'
 import { NavBar } from '../src/components/common/NavBar'
 import { Box, createTheme, ThemeProvider } from '@mui/material'
+import NextNProgress from "nextjs-progressbar";
 import { Footer } from '../src/components/common/Footer'
 
 const theme = createTheme({
@@ -19,6 +20,11 @@ const theme = createTheme({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <NextNProgress
+        color="#3F3C9A"
+        stopDelayMs={0}
+        height={5}
+      />
       <HeadPageTitle />
       <NavBar />
       <Box component='main'>        
