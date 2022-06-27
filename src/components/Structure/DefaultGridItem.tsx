67d239f3b792915158b209c468c251b42aa18262
Guilherme Hammes <gffhammes@gmail.com>
2022-06-27 12:55:30 -0300

    refactor: create grid item component

---
+++ src/components/Structure/DefaultGridItem.tsx
@@ -0,0 +1,61 @@
+import { Box, Container, Grid, Stack, SxProps, Theme, Typography } from '@mui/material'
+import Image from 'next/image'
+import React from 'react'
+
+interface IDefaultGridItemProps {
+  image: string;
+  text: string;
+  height?: boolean;
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+}
+
+const sxBox: SxProps<Theme> = {
+  width: '100%',
+  overflow: 'hidden',
+  '& > *': {
+    transition: '500ms ease all'
+  },
+  '&:hover > *': {
+    transform: 'translateY(-50%)'
+  },
+}
+
+const sxStack: SxProps<Theme> = {
+  width: '100%',
+  height: '200%'
+}
+
+const sxImage: SxProps<Theme> = {
+  position: 'relative', width: '100%', height: '100%'
+}
+
+const sxTextBlock: SxProps<Theme> = {
+  width: '100%', height: '100%', color: 'white', display: 'flex', backgroundColor: 'primary.main'
+}
+
+export const DefaultGridItem = ({ image, text, height=false, xs, sm, md, lg, xl }: IDefaultGridItemProps) => {
+
+  return (
+    <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl} >
+      <Box sx={{ ...sxBox, height: height ? '100%' : 'unset', aspectRatio: height ? 'unset' : '1 / 1' }}>
+        <Stack sx={sxStack}>
+          <Box sx={sxImage}>
+            <Image
+              src={image}
+              alt={text}
+              layout='fill'
+              objectFit='cover'
+            />
+          </Box>
+          <Box sx={sxTextBlock}>
+            <Typography textAlign='center' fontSize={20} sx={{ m: 'auto', maxWidth: '15ch' }}>{text}</Typography>
+          </Box>
+        </Stack>
+      </Box>
+    </Grid>
+  )
+}
