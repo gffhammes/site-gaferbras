2786a5e7558ba96b387327503e88b60747dc5b0f
Guilherme Hammes <gffhammes@gmail.com>
2022-09-16 12:35:05 -0300

    refactor: change backgroudn color and logo size

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,41 +1,60 @@
-import { Box, Container, Stack, Typography } from '@mui/material'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { useBreakPoint } from '../../../hooks/useBreakPoint'
-import { NavLinks } from './NavLinks'
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useBreakPoint } from "../../../hooks/useBreakPoint";
+import { NavLinks } from "./NavLinks";
 
-interface IProps { }
+interface IProps {}
 
 const Logo = () => {
   return (
-    <Link href='/' passHref>
-      <a>                
-        <Box sx={{ position: 'relative', height: { xs: '3rem', sm: '4rem', md: '6rem' }, width: '10rem' }}>
+    <Link href="/" passHref>
+      <a>
+        <Box
+          sx={{
+            position: "relative",
+            height: { xs: "3rem", sm: "4rem", md: "7rem" },
+            width: "14rem",
+          }}
+        >
           <Image
-            src='/images/gaferbras-logo.png'
-            alt='Gaferbras'
-            layout='fill'
-            objectFit='contain'
-            objectPosition='left'
+            src="/images/gaferbras-logo.png"
+            alt="Gaferbras"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
           />
         </Box>
       </a>
     </Link>
-  )
-}
+  );
+};
 
 export const NavBar = (props: IProps) => {
   const { mdSize } = useBreakPoint();
 
   return (
-    <Box bgcolor='white' component='header' sx={{ position: mdSize ? 'relative' : 'sticky', top: 0, zIndex: 999, width: '100%' }}>
+    <Box
+      bgcolor="white"
+      component="header"
+      sx={{
+        position: mdSize ? "relative" : "sticky",
+        top: 0,
+        zIndex: 999,
+        width: "100%",
+      }}
+    >
       <Container sx={{ py: 2 }}>
-        <Stack direction='row' alignItems='center' justifyContent='space-between'>  
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
           <Logo />
           <NavLinks />
         </Stack>
       </Container>
     </Box>
-  )
-}
+  );
+};
